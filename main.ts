29a311fbba6d84eda89f90e8d1a5ca1c1aed1529
LW22xShapeshifter <112982517+LW22xShapeshifter@users.noreply.github.com>
2022-09-13 12:56:12 -0600

    Added code to button A and B, and intro

--- main.ts
+++ main.ts
@@ -1,18 +1,44 @@
 input.onButtonPressed(Button.A, function () {
-	
+    TruthDare = randint(0, 1)
+    if (TruthDare == 0) {
+        basic.showString("Truth")
+    } else {
+        basic.showString("Dare")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    PickPerson = randint(0, 7)
+    if (PickPerson == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else if (PickPerson == 1) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (PickPerson == 2) {
+        basic.showArrow(ArrowNames.East)
+    } else if (PickPerson == 3) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (PickPerson == 4) {
+        basic.showArrow(ArrowNames.South)
+    } else if (PickPerson == 5) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (PickPerson == 6) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        basic.showArrow(ArrowNames.NorthWest)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     RandomNumber = randint(0, 2)
     if (RandomNumber == 0) {
         basic.showString("-Unclear-")
     } else if (RandomNumber == 1) {
-        basic.showString("no")
+        basic.showString("No")
     } else {
-        basic.showString("yes")
+        basic.showString("Yes")
     }
 })
 let RandomNumber = 0
-basic.showString("8BALL! SHAKE")
-basic.forever(function () {
-	
-})
+let PickPerson = 0
+let TruthDare = 0
+basic.showString("8Ball!")
+basic.pause(100)
+basic.showString("Press A")
